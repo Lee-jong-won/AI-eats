8ea808a016c239e8521171bf7e731abddc4cf1a9
Lee-jong-won <dlwhddnjs951@naver.com>
2024-02-13 22:19:33 +0900

    240213

---
+++ AI-Eats/src/main/java/Personal/AIEats/AiEatsApplication.java
@@ -0,0 +1,13 @@
+package Personal.AIEats;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AiEatsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AiEatsApplication.class, args);
+	}
+
+}
---
+++ AI-Eats/src/test/java/Personal/AIEats/AiEatsApplicationTests.java
@@ -0,0 +1,13 @@
+package Personal.AIEats;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AiEatsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
